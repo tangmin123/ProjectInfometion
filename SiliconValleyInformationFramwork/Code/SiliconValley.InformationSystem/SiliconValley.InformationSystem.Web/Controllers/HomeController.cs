104893fbdc4d620fd7dc9b2ba334ddaf53018636
tangmin <tangmin@qq.com>
2019-05-08 11:36:36 +0800

    cc

---
+++ SiliconValleyInformationFramwork/Code/SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SiliconValley.InformationSystem.Business;
+
+namespace SiliconValley.InformationSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Business.Base_SysManage.Base_UserBusiness buser = new Business.Base_SysManage.Base_UserBusiness();
+            // var list= buser.GetList();
+            //return View(list);
+            Business.Base_SysManage.Base_SysRoleBusiness brole = new Business.Base_SysManage.Base_SysRoleBusiness();
+            var list = brole.GetList();
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
