f83d38e0f4425f0d1943e283a12113d95e2d9897
dalei <dalei@qq.com>
2019-05-08 14:00:01 +0800

    chuang jian dalei fen zhi

---
+++ SiliconValleyInformationFramwork/Code/SiliconValley.InformationSystem/SiliconValley.InformationSystem.Web/Areas/dalei/Controllers/Demo1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconValley.InformationSystem.Web.Areas.dalei.Controllers
+{
+    public class Demo1Controller : Controller
+    {
+        // GET: dalei/Demo1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
